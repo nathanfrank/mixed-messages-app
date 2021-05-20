50e3b72863e25ab471b8e0a78c70e8f648b971b2
nathanfrank <nfrank@lululemon.com>
2021-05-20 07:38:05 -0700

    Mixed Messages Project description comments

---
+++ mixedmessagesapp.js
@@ -0,0 +1,4 @@
+// Daily motivation generator
+// Want to function similar to "I Am Sober" app's quote generator
+// Random background image + today's date + random motivational quote
+// Built with JavaScript (likely some HTML/CSS)

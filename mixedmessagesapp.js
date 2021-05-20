04c38f7af2f699ed039a0f7910a1e72fca2728a2
nathanfrank <nfrank@lululemon.com>
2021-05-20 07:41:17 -0700

    List high level steps of project

--- mixedmessagesapp.js
+++ mixedmessagesapp.js
@@ -2,3 +2,7 @@
 // Want to function similar to "I Am Sober" app's quote generator
 // Random background image + today's date + random motivational quote
 // Built with JavaScript (likely some HTML/CSS)
+// 1st step -> generate random quote
+// 2nd step -> display today's date above quote
+// 3rd step -> generate random background image
+// 4th step -> style with CSS
